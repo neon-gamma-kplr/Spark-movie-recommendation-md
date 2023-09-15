0d6f3836d4a22d9aae01ce943f2d0815e67aec2b
neon-gamma-kplr <130390722+neon-gamma-kplr@users.noreply.github.com>
2023-09-15 12:01:51 +0000

    début MAJ app.py

--- app/app.py
+++ app/app.py
@@ -1,84 +1,75 @@
-import Flask
-import Blueprint` pour créer l'application web.
-    - `render_template` pour charger les modèles de templates.
-    - `json` pour manipuler les données au format JSON.
-    - `findspark` pour trouver et initialiser Spark.
-    - `SparkContext` et `SparkSession` pour travailler avec Spark.
-    - `RecommendationEngine` (supposons qu'il s'agit d'un fichier `engine.py`) pour gérer les recommandations.
-2. Créez un Blueprint Flask :
-```python
+import Flask # pour créer l'application web
+import Blueprint # pour créer l'application web
+import render_template # pour charger les modèles de templates
+import json # pour manipuler les données au format JSON
+import findspark # pour trouver et initialiser Spark
+import SparkContext # pour travailler avec Spark
+import SparkSession # pour travailler avec Spark
+from engine import RecommendationEngine # pour gérer les recommandations
+
+
+# Créez un Blueprint Flask
 main = Blueprint('main', __name__)
-```
 
-3. Initialisez Spark :
-```python
+# Initialisez Spark
 findspark.init()
-```
 
-4. Définissez la route principale ("/") : 
-```python
+
+# Définissez la route principale ("/")
 @main.route("/", methods=["GET", "POST", "PUT"])
 def home():
     return render_template("index.html")
-```
 
-5. Définissez la route pour récupérer les détails d'un film : 
-```python
+
+# Définissez la route pour récupérer les détails d'un film
 @main.route("/movies/<int:movie_id>", methods=["GET"])
 def get_movie(movie_id):
     # Code pour récupérer les détails du film avec l'id spécifié
     # et renvoyer les données au format JSON
-    ```
 
-6. Définissez la route pour ajouter de nouvelles évaluations pour les films : 
-```python
+
+# Définissez la route pour ajouter de nouvelles évaluations pour les films
 @main.route("/newratings/<int:user_id>", methods=["POST"])
 def new_ratings(user_id):
     # Code pour vérifier si l'utilisateur existe déjà
     # Si l'utilisateur n'existe pas, créez-le
     # Récupérez les évaluations depuis la requête et ajoutez-les au moteur de recommandation
     # Renvoyez l'identifiant de l'utilisateur si c'est un nouvel utilisateur, sinon renvoyez une chaîne vide
-    ```
 
-7. Définissez la route pour ajouter des évaluations à partir d'un fichier : 
-```python
+
+# Définissez la route pour ajouter des évaluations à partir d'un fichier
 @main.route("/<int:user_id>/ratings", methods=["POST"])
 def add_ratings(user_id):
     # Code pour récupérer le fichier téléchargé depuis la requête
     # Lisez les données du fichier et ajoutez-les au moteur de recommandation
     # Renvoyez un message indiquant que le modèle de prédiction a été recalculé
-    ```
 
-8. Définissez la route pour obtenir la note prédite d'un utilisateur pour un film : 
-```python
+
+# Définissez la route pour obtenir la note prédite d'un utilisateur pour un film
 @main.route("/<int:user_id>/ratings/<int:movie_id>", methods=["GET"])
 def movie_ratings(user_id, movie_id):
     # Code pour prédire la note de l'utilisateur pour le film spécifié
     # Renvoyez la note prédite au format texte
-    ```
 
-9. Définissez la route pour obtenir les meilleures évaluations recommandées pour un utilisateur : 
-```python
+
+# Définissez la route pour obtenir les meilleures évaluations recommandées pour un utilisateur
 @main.route("/<int:user_id>/ratings/<int:movie_id>", methods=["GET"])
 def movie_ratings(user_id, movie_id):
     # Code pour prédire la note de l'utilisateur pour le film spécifié
     # Renvoyez la note prédite au format texte
-    ```
 
-10. Définissez la route pour obtenir les évaluations d'un utilisateur : 
-```python
+
+# Définissez la route pour obtenir les évaluations d'un utilisateur
 @main.route("/ratings/<int:user_id>", methods=["GET"])
 def get_ratings_for_user(user_id):
     # Code pour récupérer les évaluations de l'utilisateur spécifié
     # Renvoyez les évaluations au format JSON
-    ```
-    
-11. Créez une fonction `create_app(spark_context, movies_set_path, ratings_set_path)` pour créer l'application Flask :
- ```python
+
+
+# Créez une fonction `create_app(spark_context, movies_set_path, ratings_set_path)` pour créer l'application Flask
  def create_app(spark_context, movies_set_path, ratings_set_path):
     # Code pour initialiser le moteur de recommandation avec le contexte Spark et les jeux de données
     # Créez une instance de l'application Flask
     # Enregistrez le Blueprint "main" dans l'application
     # Configurez les options de l'application Flask
     # Renvoyez l'application Flask créée
-    ```
